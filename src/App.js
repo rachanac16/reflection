d206f0b41339ea0f32f2cf98fc0f2ddd030b4f04
Rachana Chaudhari <rachanachaudhari16@gmail.com>
2022-09-25 17:39:26 -0400

    Bare bones

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This website is underdevelop. Wait for it, or not.
+      </header>
+    </div>
+  );
+}
+
+export default App;
