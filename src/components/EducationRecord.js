8068d996cb80301bc0e2036e0ab0558780e47990
Rachana Chaudhari <rachanachaudhari16@gmail.com>
2024-04-22 00:42:02 -0500

    Education page done

--- src/components/EducationRecord.js
+++ src/components/EducationRecord.js
@@ -5,20 +5,7 @@ function EducationRecord(props) {
   const [record, setRecord] = useState(props.record);
 
   return (
-    <Grid item container xs={12} md={6} lg={6} className="bg-white">
-      <Grid
-        item
-        container
-        direction="column"
-        xs={0}
-        md={1}
-        lg={1}
-        className="bg-primary"
-      >
-        <Grid item xs={2} md={2} lg={2} className="bg-white"></Grid>
-        <Grid item xs={10} md={8} lg={8}></Grid>
-        <Grid item xs={0} md={2} lg={2} className="bg-white"></Grid>
-      </Grid>
+    <Grid item container xs={12} md={6} lg={6} className="bg-secondary" p={4}>
       <Grid
         item
         container
@@ -26,18 +13,16 @@ function EducationRecord(props) {
         xs={3}
         md={3}
         lg={3}
-        className="bg-primary"
+        className="bg-secondary"
       >
         <Grid item xs={2} md={4} lg={4}>
           <Avatar
             alt={record.universityName}
             src={record.icon}
-            sx={{ height: "100%", width: "100%" }}
-            variant="square"
+            sx={{ height: "10rem", width: "10rem", border:"0.5rem solid var(--primary)" }}
+            variant="circle"
           />
         </Grid>
-        <Grid item direction="column" xs={10} md={6} lg={6}></Grid>
-        <Grid item xs={0} md={2} lg={2} className="bg-white"></Grid>
       </Grid>
       <Grid
         item
@@ -46,26 +31,36 @@ function EducationRecord(props) {
         xs={9}
         md={8}
         lg={8}
-        className="padding2"
-        spacing={2}
+        className="padding2 padding-left2"
+        spacing={1}
       >
-        <Grid item className="sub-header-2">
+        <Grid item className="sub-header display-flex-center">
           {record.universityName}
         </Grid>
-        <Grid item className="content-italic color-grey">
-          {record.date}
-        </Grid>
-        <Grid item className="content">
+        <Grid item className="content body display-flex-center">
           {record.degree}
         </Grid>
-        <Grid item className="content">
+        <Grid item>
+        <hr
+          style={{
+              color: "var(--primary)",
+              backgroundColor: "var(--primary)",
+              height: 2,
+              width: "100%",
+          }}
+        />
+        </Grid>
+        <Grid item className="content-bold body">
+          {record.date}
+        </Grid>
+        <Grid item className="content body">
           CGPA: {record.cgpa}
         </Grid>
-        <Grid item className="content">
+        <Grid item className="content-small body">
           {record.desc}
         </Grid>
-        <Grid item className="content">
-          <div className="content-italic">RELEVENT COURSEWORK</div>
+        <Grid item className="content body">
+          <div>Relevant Coursework</div>
           <ul>
             {record.relevantCoursework.map((course) => (
               <li>{course}</li>
@@ -73,6 +68,7 @@ function EducationRecord(props) {
           </ul>
         </Grid>
       </Grid>
+      <Grid item xs={1} md={1} lg={1}></Grid>
     </Grid>
   );
 }
